fe412805273206ee21d97f573dfcb83803ae86b0
XeduR <eetu.rantanen@spyric.com>
2021-06-17 23:43:24 +0300

    Gone Diggin' - minor changes
    - Remove some white space.
    - Change the cover image to match LD's version.
    - Change buttonAudio from newImageRect to newRect.

--- Games/Gone Diggin/main.lua
+++ Games/Gone Diggin/main.lua
@@ -55,18 +55,18 @@ local function generateText( group, text, x, y, anchorX, anchorY, fontSize )
     local textObject = display.newGroup()
     textObject.x, textObject.y = x, y
     group:insert(textObject)
-    
+
     textObject.back = display.newText( textObject, text, 1, 1, font, fontSize )
     textObject.back.anchorX, textObject.back.anchorY = anchorX, anchorY
     textObject.back:setFillColor(0)
     textObject.front = display.newText( textObject, text, 0, 0, font, fontSize )
     textObject.front.anchorX, textObject.front.anchorY = anchorX, anchorY
-    
+
     function textObject:text(s)
         textObject.back.text = s
         textObject.front.text = s
     end
-    
+
     return textObject
 end
 
@@ -101,7 +101,7 @@ logoT1()
 -- Audio setup:
 
 -- Creating audio button and function for controlling audio levels in the game.
-local buttonAudio = display.newImageRect( groupUI, "images/logo.png", 48, 48 )
+local buttonAudio = display.newRect( groupUI, 0, 0, 48, 48 )
 buttonAudio.x, buttonAudio.y = screen.maxX - buttonAudio.width*0.5 - 4, screen.minY + buttonAudio.height*0.5 + 4
 buttonAudio.state = true
 buttonAudio.fillOn = {
@@ -165,7 +165,7 @@ local function createEmitter( filename, x, y )
     local emitterData = f:read( "*a" )
     f:close()
     local emitterParams = json.decode( emitterData )
-     
+
     local emitter = display.newEmitter( emitterParams )
     groupEmitter:insert(emitter)
     emitter:stop()
@@ -184,7 +184,7 @@ local sheetOptions = {
     width = 64,
     height = 64,
     numFrames = 4,
-    
+
     sheetContentWidth = 64,
     sheetContentHeight = 256
 }
@@ -216,7 +216,7 @@ local function update()
         gameover()
         return
     end
-    
+
     local r, g = 0, 1
     if timeLeft > 0.5 then
         r = (1 - timeLeft)*2
@@ -224,7 +224,7 @@ local function update()
         r = 1
         g = timeLeft*2
     end
-    
+
     if timeLeft < 0.2 then
         diggingText.text = "Dig faster! Hurry!"
     elseif timeLeft < 0.5 then
@@ -232,7 +232,7 @@ local function update()
     elseif timeLeft < 0.75 then
         diggingText.text = "Dig deeper!"
     end
-    
+
     diggingMeter.width = diggingMeter.maxWidth*timeLeft
     diggingMeter:setFillColor( r, g, 0 )
 end
@@ -242,7 +242,7 @@ local function generateWorld( seed )
     if type( seed ) == "number" then
         math.randomseed( seed )
     end
-    
+
     if ring then
         for i = 1, #ring do
             for j = 1, #ring[i].overlay do
@@ -256,11 +256,11 @@ local function generateWorld( seed )
     end
     groupWorldBack.rotation = 0
     groupWorldFront.rotation = 0
-    
+
     display.setDefault( "background", 96/255, 203/255, 239/255 )
     bgShading.alpha = 0
     maskTop.alpha = 0
-    
+
     ring, ringScales = newRing.create( ringParameters, startingLayer )
     currentDifficulty = ringParameters.ringCount - ringParameters.surfaceLayers - startingLayer
     activeLayer = startingLayer
@@ -288,9 +288,9 @@ local function generateWorld( seed )
     end
     groupWorldBack.x, groupWorldBack.y = screen.centreX, screen.maxY + (groupWorldBack.height - scaleFactor*ringParameters.thickness*2 )*0.5
     groupWorldFront.x, groupWorldFront.y = groupWorldBack.x, groupWorldBack.y
-    
+
     -- ring[activeLayer].overlay[activeColumn].bitmask = 0
-    
+
     player.y = groupWorldBack.y - groupWorldBack.height*0.5 + playerStartY + 14
     player.startY = player.y
     player.isVisible = true
@@ -312,7 +312,7 @@ local function movePlayer( direction )
                     transition.to( howToPlay, { time=350, alpha=0 } )
                     transition.to( logo, { time=350, y=screen.minY - logo.height*2 } )
                 end
-                
+
                 canMove = false
                 currentDifficulty = currentDifficulty + 1
                 if bgShading.alpha < 1 then
@@ -325,7 +325,7 @@ local function movePlayer( direction )
                         display.setDefault( "background", 157/255, 120/255, 73/255  )
                     end
                 end
-                
+
                 for i = 1, #ring do
                     local ringOverlay = ring[i]
                     local ringBack = ring[i].back
@@ -342,7 +342,7 @@ local function movePlayer( direction )
                         transition.to( ringOverlay, { time=transitionTime, xScale=scale, yScale=scale, transition=easing.inSine })
                         transition.to( ringBack, { time=transitionTime, xScale=scale, yScale=scale, transition=easing.inSine  })
                     end
-                    
+
                 end
                 activeLayer = activeLayer+1
                 if activeLayer > ringParameters.ringCount then
@@ -368,7 +368,7 @@ local function movePlayer( direction )
                     nextColumn = 1
                 end
             end
-            
+
             if not ring[activeLayer].overlay[nextColumn].isPassable then
                 impassable = true
             else
@@ -379,20 +379,20 @@ local function movePlayer( direction )
                 didMove = true
             end
         end
-        
+
         if didMove then
             local overlay = ring[activeLayer].overlay[activeColumn]
             local backdrop = ring[activeLayer].backdrop[activeColumn]
             if not overlay.isVisited then
                 overlay.isVisited = true
                 -- overlay.isVisible = false
-                
+
                 local whichAudio
-                
+
                 local activeEmitter = emitterGround
                 if overlay.isGold then
                     whichAudio = sfx["gold"]
-                    
+
                     goldCount = goldCount+1
                     goldCounter:text(goldCount)
                     goldCounter.front:setFillColor(1,210/255,0)
@@ -403,13 +403,13 @@ local function movePlayer( direction )
                         bonusTime = bonusTime + extraTimeFromGold
                     end
                     activeEmitter = emitterGold
-                    
+
                     -- To make the game slightly easier at immediate start, only start
                     -- the timer after the player has already picked a few gold segments.
                     if not timerStarted and goldCount > 2 then
                         timerStarted = true
                         startTime = getTimer()
-                        
+
                         transition.to( diggingCounter, {time=350, y=screen.maxY-64, transition=easing.inSine} )
                         diggingMeter:setFillColor(0,1,0)
                         Runtime:addEventListener( "enterFrame", update )
@@ -419,18 +419,18 @@ local function movePlayer( direction )
                     whichAudio = sfx["ground"]
                 end
                 audio.play( whichAudio )
-                    
+
                 ----------------------------------------------------------------------------------------------------------------
                 -- We can get by using simple bitmasking value calculations since the player can only move left, right or down.
                 -- This means that we only ever need to update the new segment where the player moves to, as well as the segment
                 -- they were in before the move. Also, the game has a guaranteed impassable segment on every ring, so the player
                 -- can't move to a visited segment from a previously unvisited segment.
-                
+
                 -- This means that all we need to do is take the bitmask value for the direction from where the player came from
                 -- and apply this to the new segment's fill. Then we update the previous segment's fill by adding the direction
                 -- to where the player moved to.
                 ----------------------------------------------------------------------------------------------------------------
-                
+
                 local particleOffsetX = 78
                 local dirComing, dirGoing
                 if direction == "left" then
@@ -449,18 +449,18 @@ local function movePlayer( direction )
                     activeEmitter.x = screen.centreX
                     activeEmitter.y = player.y + 40
                 end
-                
+
                 if previousSegment then
                     previousSegment.bitmask = previousSegment.bitmask + dirGoing
                     previousSegment.fill = diggingPath[previousSegment.bitmask]
                 end
                 backdrop.bitmask = dirComing
                 backdrop.fill = diggingPath[dirComing]
-                
+
                 if ring[activeLayer].isVisible then
                     activeEmitter:start()
                 end
-                
+
                 transition.to( overlay, { time=transitionTime, alpha=0, onComplete=function()
                     overlay.isVisible = false
                     overlay.alpha = 1
@@ -477,19 +477,19 @@ function gameover()
     Runtime:removeEventListener( "enterFrame", update )
     Runtime:removeEventListener( "key", keyEvent )
     audio.play( sfx["gameover"] )
-    
+
     goldCounterCopy.x, goldCounterCopy.y = goldCounterCopy.xStart, goldCounterCopy.yStart
     goldCounterCopy:text(goldCount)
     goldCounterCopy.isVisible = true
     goldCounter.isVisible = false
-    
+
     transition.to( goldCounterCopy, { delay=250, time=gameoverTransitionTime*0.5, x=240, y=88+goldCounterCopy.height*0.5, onComplete=function()
         transition.to( player, { time=350, y=player.startY } )
         transition.from( highscore, { time=100, xScale=1.5, yScale=1.5 })
         transition.from( lastScore, { time=100, xScale=1.5, yScale=1.5 })
         goldCounterCopy.isVisible = false
-        
-        
+
+
         if goldCount > highscoreVal then
             highscoreVal = goldCount
             highscore.front:setFillColor(1,210/255,0)
@@ -528,7 +528,7 @@ function keyEvent( event )
             elseif activeKey == "s" or activeKey == "down" then
                 movePlayer("down")
             elseif activeKey == "w" or activeKey == "up" then
-                
+
             elseif activeKey == "q" then
                 gameover()
             end
