b0ac9ae0af142f93bda7a5f61c8eb33a1f787fec
XeduR <eetu.rantanen@spyric.com>
2022-04-02 06:29:12 +0300

    Last Stand: add placeholder zombie module
    Going to play around with OOP this time with creating OO zombies.
    
    Committing this stub to work on it later on a different computer.

---
+++ Games/Last Stand/classes/zombie.lua
@@ -0,0 +1,13 @@
+local zombie = {}
+
+-- https://docs.coronalabs.com/api/library/graphics/newImageSheet.html
+-- https://docs.coronalabs.com/api/library/display/newSprite.html
+
+function zombie.new( parent, x, y, player )
+    -- local newZombie = display.newSprite()
+    
+    
+    -- return newZombie
+end
+
+return zombie
