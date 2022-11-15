1a1aad79122663efc2b309103085fc08e594b8a9
XeduR <eetu.rantanen@spyric.com>
2022-11-15 13:42:46 +0200

    Add word by word split to rolling text function

--- Unassorted Functions & Concepts/Rolling text/main.lua
+++ Unassorted Functions & Concepts/Rolling text/main.lua
@@ -1,16 +1,42 @@
--- Create a standard Solar2D newText display object and reveal it character by character.
-local function newRollingText( text, x, y, font, fontSize, revealTime )
-	if type( text ) == "string" then
-		local length = text:len()
+-- Create a standard Solar2D newText display object and reveal it character by character or word by word.
+local function newRollingText( params )
+	if type( params ) ~= "table" then
+        print( "WARNING: bad argument #1 to 'newRollingText' (table expected, got nil)" )
+    else
+        local text = params.text or ""
+        local x = params.x or 0
+        local y = params.y or 0
+        local font = params.font or native.systemFont
+        local fontSize = params.fontSize or 18
+        local reveal = params.reveal or "word"
+        local length, word
+
+        -- NB! With "word by word" separation, all hidden special characters,
+        -- like line breaks, will be missed/ignored.
+        if reveal == "word" then
+            word = {}
+            -- Split the string into words.
+            for w in text:gmatch("%S+") do
+                word[#word+1] = w
+            end
+            length = #word
+        else
+            length = text:len()
+        end
+
 		if length > 0 then
 			local object = display.newText( "", x or 0, y or 0, font or native.systemFont, fontSize or 18 )
 			object.anchorX, object.anchorY = 0, 0
-			
-			local time = revealTime and revealTime/length
+
+            local time = (params.time or 1000)/length
 			if time then
 				local n = 1
 				timer.performWithDelay( time, function()
-					object.text = text:sub(1,n)
+                    if word then
+                        object.text = object.text .. " " .. word[n]
+                    else
+                        object.text = text:sub(1,n)
+                    end
 					n = n+1
 				end, length )
 			else
@@ -21,4 +47,12 @@ local function newRollingText( text, x, y, font, fontSize, revealTime )
 	end
 end
 
-local sampleText = newRollingText( "Here's a cool rolling text sample:\n\nHello world!", 40, 80, native.systemFont, 24, 1200 )
+local sampleText = newRollingText({
+    text = "Here's a cool rolling text sample:\n\nHello world!",
+    x = 40,
+    y = 80,
+    font = native.systemFont,
+    fontSize = 24,
+    reveal = "character",
+    time = 1200
+})
