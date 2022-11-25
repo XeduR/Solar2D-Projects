40ce7be74ff01ab34e82583115516e0d93b4d300
XeduR <eetu.rantanen@spyric.com>
2022-11-25 19:28:20 +0200

    rng: update style and comments
    The module's functionality remains unchanged.

--- Pseudorandom Number Generator/main.lua
+++ Pseudorandom Number Generator/main.lua
@@ -1,50 +1,89 @@
+-------------------------------------------------------------------------
+--                                                                     --
+--    ooooooo  ooooo                 .o8              ooooooooo.       --
+--     `8888    d8'                 "888              `888   `Y88.     --
+--       Y888..8P     .ooooo.   .oooo888  oooo  oooo   888   .d88'     --
+--        `8888'     d88' `88b d88' `888  `888  `888   888ooo88P'      --
+--       .8PY888.    888ooo888 888   888   888   888   888`88b.        --
+--      d8'  `888b   888    .o 888   888   888   888   888  `88b.      --
+--    o888o  o88888o `Y8bod8P' `Y8bod88P"  `V88V"V8P' o888o  o888o     --
+--                                                                     --
+--  © 2022 Eetu Rantanen                Last Updated: 25 November 2022 --
+-------------------------------------------------------------------------
+--  License: MIT                                                       --
+-------------------------------------------------------------------------
+-- This sample project is written for Solar2D, but the rng module itself
+-- is written in pure Lua and doesn't require Solar2D to run.
+
+-- There is also an interactive online tool with further info at:
+-- https://www.xedur.com/demos/Pseudorandom%20Number%20Generator/
+------------------------------------------------------------------------
+
+display.setStatusBar( display.HiddenStatusBar )
+
 local rng = require("rng")
 
--- This sample project is written for Solar2D, but the rng module itself is written in pure Lua and doesn't require Solar2D to run.
-
--- There is also an interactive online tool with further info at: https://www.xedur.com/demos/Pseudorandom%20Number%20Generator/
-
-------------------------------------------------------------------------------------------------------------------------------------
-
+------------------------------------------------------------------------
 -- You can edit these values to change the grid size and random generation:
-local randomseed = 51251612123456
-local borderPadding = 20
-local desiredGridSize = 500
 
-------------------------------------------------------------------------------------------------------------------------------------
+-- local randomseed = os.time() -- Use a dynamic randomseed
+local randomseed = 5125161 -- Set a hardcoded randomseed
+
+local desiredGridSize = 200 -- width and height in pixels.
+local borderPadding = 20
+
+------------------------------------------------------------------------
 
 -- Localise functions.
 local random = rng.random
 local newRect = display.newRect
 
 -- Cap the grid size so that it fits inside the screen with the appropriate border padding.
-local gridSize = math.min( desiredGridSize, display.actualContentWidth-borderPadding*2, display.actualContentHeight-borderPadding*2 )
+local gridSize = math.min(
+	desiredGridSize,
+	display.actualContentWidth-borderPadding*2,
+	display.actualContentHeight-borderPadding*2
+)
 local xStart = math.floor(display.contentCenterX-gridSize*0.5)
 local yStart = math.floor(display.contentCenterY-gridSize*0.5)
 rng.randomseed( randomseed )
 
-------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------
 
 -- Draw the grid.
 local currentRow = 0
 local function drawRow()
-    for i = 1, 10 do
-        currentRow = currentRow+1
-        for column = 1, gridSize do 
-            local dot = newRect( xStart+column, yStart+currentRow, 1, 1 )
-            dot.anchorX, dot.anchorY = 0, 0
-            dot:setFillColor( random() )
-        end
-    end
-    if currentRow < gridSize then
-        timer.performWithDelay( 5, drawRow )
-    end
+	for _ = 1, 10 do
+		currentRow = currentRow+1
+		for column = 1, gridSize do
+			local dot = newRect( xStart+column, yStart+currentRow, 1, 1 )
+			dot.anchorX, dot.anchorY = 0, 0
+			dot:setFillColor( random() )
+		end
+	end
+	if currentRow < gridSize then
+		timer.performWithDelay( 1, drawRow )
+	end
 end
 drawRow()
 
-------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------
 
 -- Create UI texts.
 local yMax = display.actualContentHeight - display.screenOriginY
-local title = display.newText( "randomseed = " .. randomseed, display.contentCenterX, display.screenOriginY + yStart*0.5, native.systemFont, 20 )
-local dotCount = display.newText( "Number of random dots: " .. gridSize*gridSize, display.contentCenterX, yStart + gridSize + (yMax - (yStart + gridSize))*0.5, native.systemFont, 20 )
+
+local title = display.newText({
+	text = "randomseed = " .. randomseed,
+	x = display.contentCenterX,
+	y = display.screenOriginY + yStart*0.5,
+	font = native.systemFont,
+	fontSize = 20
+})
+
+local dotCount = display.newText({
+	text = "Number of random dots: " .. gridSize*gridSize,
+	x = display.contentCenterX,
+	y = yStart + gridSize + (yMax - (yStart + gridSize))*0.5,
+	font = native.systemFont,
+	fontSize = 20
+})
--- Pseudorandom Number Generator/rng.lua
+++ Pseudorandom Number Generator/rng.lua
@@ -8,28 +8,23 @@
 --      d8'  `888b   888    .o 888   888   888   888   888  `88b.      --
 --    o888o  o88888o `Y8bod8P' `Y8bod88P"  `V88V"V8P' o888o  o888o     --
 --                                                                     --
---  © 2021 Eetu Rantanen                    Last Updated: 24 June 2021 --
+--  © 2022 Eetu Rantanen                Last Updated: 25 November 2022 --
 -------------------------------------------------------------------------
 --  License: MIT                                                       --
 -------------------------------------------------------------------------
-
------------------------------------------------------------------------
 -- Pseudorandom number generation using the linear congruential method:
------------------------------------------------------------------------
--- An interactive online tool demonstrating the random distribution is available at:
+--
+-- Lua 5.1's math.random implementation is platform dependent, meaning
+-- it may return different results on different platforms, even when
+-- using the same randomseed.
+--
+-- This module's random function will generate consistent pseudorandom
+-- numbers, but it is slightly slower than Lua's random function.
+--
+-- You can see the pseudorandom number genration in action over at:
 -- https://www.xedur.com/demos/Pseudorandom%20Number%20Generator/
---
--- This module is intended to be used for when you need to generate
--- platform independent pseudorandom numbers in your Lua applications.
--- Lua's implementation of math.random() (at least for Lua 5.1, which
--- affects Solar2D) is platform specific and its outputs will depend
--- on what platform you are running it on.
---
--- rng.random() and rng.randomseed() are used exactly like their math
--- counterparts, but these functions run a bit slower. The difference,
--- however, is negligible unless you are calling rng.random() hundreds
--- or thousands of times per frame.
------------------------------------------------------------------------
+-------------------------------------------------------------------------
+
 local rng = {}
 
 -- Localised math functions.
@@ -43,23 +38,27 @@ local c = 1013904223
 local m = 2^32
 local seed = 12345
 
+-------------------------------------------------------------------------
+
 -- Set a new initial random seed.
 function rng.randomseed(n)
-    if _type(n) == "number" then
-        seed = _floor(_abs(n+0.5))
-    else
-        print( "WARNING: bad argument to 'randomseed' (number expected, got ".._type(n)..")" )
-    end
+	if _type(n) == "number" then
+		seed = _floor(_abs(n+0.5))
+	else
+		print( "WARNING: bad argument to 'randomseed' (number expected, got ".._type(n)..")" )
+	end
 end
 
--- Generate a pseudorandom number.
+-- Generate a pseudorandom number using the linear congruential method.
 function rng.random(x,y)
-    seed = (a * seed + c) % m
-    local r = seed / m
-    -- With no arguments,  return a pseudorandom number (fraction) between 0 and 1.
-    -- With one argument,  return a pseudorandom number (integer)  between 1 and x.
-    -- With two arguments, return a pseudorandom number (integer)  between x and y.
-    return _type(x) ~= "number" and r or _type(y) ~= "number" and _floor((x-1)*r+1.5) or _floor((y-x)*r+x+0.5)
+	seed = (a * seed + c) % m
+	local r = seed / m
+	-- With no arguments,  return a pseudorandom number (fraction) between 0 and 1.
+	-- With one argument,  return a pseudorandom number (integer)  between 1 and x.
+	-- With two arguments, return a pseudorandom number (integer)  between x and y.
+	return _type(x) ~= "number" and r or _type(y) ~= "number" and _floor((x-1)*r+1.5) or _floor((y-x)*r+x+0.5)
 end
+
+-------------------------------------------------------------------------
 
 return rng
